ac90f3e9f1a3f215e92eb1c58b8929fe1b29090a
Kuba Biel <jakubiel@o2.pl>
2022-01-06 16:54:44 +0100

    5

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const fetchData = async (searchTerm) => {
 		},
 	});
 
+	if (response.data.Error) {
+		return [];
+	}
+
 	return response.data.Search;
 };
 //debounce
@@ -16,6 +20,17 @@ const onInput = debounce(async (event) => {
 	const movies = await fetchData(event.target.value);
 	//without await we will return promise, to return array we need await, and if await we need async in front of function too
 	console.log(movies);
+	for (let movie of movies) {
+		const div = document.createElement("div");
+
+		div.innerHTML = `
+      <img src="${movie.Poster}">
+      <h1>${movie.Title} (${movie.Year})</h1>
+    `;
+		console.log(div.innerHTML);
+
+		document.querySelector("#targ").appendChild(div);
+	}
 });
 
 input.addEventListener("input", onInput);
