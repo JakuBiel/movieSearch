73d83b54e310448448d2eca093e79c0cfbbbc148
Kuba Biel <jakubiel@o2.pl>
2022-01-06 19:10:24 +0100

    5

--- index.js
+++ index.js
@@ -13,24 +13,54 @@ const fetchData = async (searchTerm) => {
 
 	return response.data.Search;
 };
-//debounce
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class='input' />
+<div class="dropdown">
+				<div class="dropdown-menu">
+					<div class="dropdown-content results"></div>
+				</div>
+			</div>
+			<div id="targ"></div>`;
+
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 const input = document.querySelector("input");
 
 const onInput = debounce(async (event) => {
 	const movies = await fetchData(event.target.value);
 	//without await we will return promise, to return array we need await, and if await we need async in front of function too
-	console.log(movies);
+	if (!movies.length) {
+		dropdown.classList.remove("is-active");
+		return;
+	}
+
+	resultsWrapper.innerHTML = "";
+	dropdown.classList.add("is-active");
+
 	for (let movie of movies) {
-		const div = document.createElement("div");
+		const option = document.createElement("a");
+		const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
 
-		div.innerHTML = `
-      <img src="${movie.Poster}">
-      <h1>${movie.Title} (${movie.Year})</h1>
+		option.classList.add("dropdown-item");
+		option.innerHTML = `
+      <img src="${imgSrc}">
+      ${movie.Title} (${movie.Year})
     `;
-		console.log(div.innerHTML);
-
-		document.querySelector("#targ").appendChild(div);
+		option.addEventListener("click", () => {
+			dropdown.classList.remove("is-active");
+			input.value = movie.Title;
+		});
+		resultsWrapper.appendChild(option);
 	}
 });
 
 input.addEventListener("input", onInput);
+
+document.addEventListener("click", (event) => {
+	if (!root.contains(event.target)) {
+		dropdown.classList.remove("is-active");
+	}
+});
